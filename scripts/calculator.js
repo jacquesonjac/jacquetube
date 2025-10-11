3c578538b075447352f1b69a9cf300008fdc2647
Jacob Malugu <jacobmalugu@gmail.com>
2025-10-11 10:39:44 +0300

    Add Calculator Functionality

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -3,6 +3,12 @@ let calculation = localStorage.getItem('calculation') || '';
 document.querySelector('.js-calculations-display').
   innerHTML = localStorage.getItem('calculation') || '';
 
+const myElement = document.querySelector('.js-sidebar-calculator');
+
+myElement.addEventListener('click', function () {
+  window.location.href = "https://nikae.net/calculator.html";
+});
+
 
 function updateCalculation(value) {
   calculation += value;
